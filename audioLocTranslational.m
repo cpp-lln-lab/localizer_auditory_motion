8c562aa44c23b49de056ceae9f30b1a6e424e873
marcobarilari <marcobarilr@gmail.com>
2020-08-25 17:20:14 +0200

    mh fix

--- audioLocTranslational.m
+++ audioLocTranslational.m
@@ -83,12 +83,12 @@ try
             thisEvent.trial_type = cfg.design.blockNames{iBlock};
             thisEvent.direction = cfg.design.directions(iBlock, iEvent);
             thisEvent.fixationTarget = cfg.design.fixationTargets(iBlock, iEvent);
-            
-            % % % WIP % % % 
-            
+
+            % % % WIP % % %
+
             % thisEvent.soundTarget = cfg.design.soundTargets(iBlock, iEvent);
-            
-            % % % WIP % % % 
+
+            % % % WIP % % %
 
             % we wait for a trigger every 2 events
             if cfg.pacedByTriggers.do && mod(iEvent, 2) == 1
--- input/equateRmsWav.m
+++ input/equateRmsWav.m
@@ -1,84 +1,84 @@
 function equateRmsWav(subjName)
 
-if nargin<1
-    subjName = input('Enter Subjects name: ','s');
-end
+    if nargin < 1
+        subjName = input('Enter Subjects name: ', 's');
+    end
 
-referenceDir = fullfile(pwd,'Static');
-subjectDir = fullfile(pwd,[ 'Motion/sub-',subjName ]);
+    referenceDir = fullfile(pwd,'Static');
+    subjectDir = fullfile(pwd,['Motion/sub-', subjName]);
 
-% reference folder (Static)
-cd(referenceDir)
-referenceWavFn = fullfile(pwd,'Static.wav');
+    % reference folder (Static)
+    cd(referenceDir);
+    referenceWavFn = fullfile(pwd, 'Static.wav');
 
-% Subject motion folder
-cd(subjectDir)
+    % Subject motion folder
+    cd(subjectDir);
 
-targetWavFn = ['sub-', subjName,'_LRL.wav'];
-runFunction (referenceWavFn,targetWavFn)
+    targetWavFn = ['sub-', subjName, '_LRL.wav'];
+    runFunction (referenceWavFn, targetWavFn)
 
-targetWavFn = [subjName,'_RLR.wav'];
-runFunction (referenceWavFn,targetWavFn)
+    targetWavFn = [subjName, '_RLR.wav'];
+    runFunction (referenceWavFn, targetWavFn)
 
-% Targets
-cd(referenceDir)
-referenceWavFn = fullfile(pwd,'Static_T.wav');
+    % Targets
+    cd(referenceDir);
+    referenceWavFn = fullfile(pwd, 'Static_T.wav');
 
-%target_wav_fn = ['M.wav'];
-cd(subjectDir)
+    %target_wav_fn = ['M.wav'];
+    cd(subjectDir);
 
-targetWavFn = [subjName,'_LRL_T.wav'];
-runFunction (referenceWavFn,targetWavFn)
+    targetWavFn = [subjName, '_LRL_T.wav'];
+    runFunction (referenceWavFn,targetWavFn)
 
-targetWavFn = [subjName,'_RLR_T.wav'];
-runFunction (referenceWavFn,targetWavFn)
+    targetWavFn = [subjName, '_RLR_T.wav'];
+    runFunction (referenceWavFn, targetWavFn)
 
-end
+    end
 
-function runFunction (referenceWavFn,targetWavFn)
-% This Script takes a file (targetWavFn) and equates its rms with
-% another reference audio file (referenceWavFn) amd gives the equated
-% wav file as an output ('final_wave.wav')
+    function runFunction (referenceWavFn, targetWavFn)
+    % This Script takes a file (targetWavFn) and equates its rms with
+    % another reference audio file (referenceWavFn) amd gives the equated
+    % wav file as an output ('final_wave.wav')
 
-%reference_wav_fn = 'R_L.wav';
-%target_wav_fn = 'L_R.wav';
+    %reference_wav_fn = 'R_L.wav';
+    %target_wav_fn = 'L_R.wav';
 
-% Get the rms of the original sound
-[referenceWav , referenceFs]= audioread(referenceWavFn);
-referenceRMS = rms(referenceWav) ;
-disp('rms of the reference wav file')
-disp(referenceRMS)
+    % Get the rms of the original sound
+    [referenceWav, referenceFs]= audioread(referenceWavFn);
+    referenceRMS = rms(referenceWav);
+    disp('rms of the reference wav file')
+    disp(referenceRMS)
 
-% Get the rms for the edited combined sound (static)
-[targetWav, ~] = audioread(targetWavFn);
-targetRms = rms(targetWav) ;
-disp('rms of the target wav file')
-disp(targetRms)
+    % Get the rms for the edited combined sound (static)
+    [targetWav, ~] = audioread(targetWavFn);
+    targetRms = rms(targetWav);
+    disp('rms of the target wav file')
+    disp(targetRms)
 
 
-% correct for the rms differences in each channel
-finalWav = [ targetWav(:,1)*(referenceRMS(1)/targetRms(1)) ...
-               targetWav(:,2)*(referenceRMS(2)/targetRms(2))] ;
+    % correct for the rms differences in each channel
+    finalWav = [targetWav(:,1)*(referenceRMS(1)/targetRms(1)) ...
+                   targetWav(:,2)*(referenceRMS(2)/targetRms(2))];
 
-% check that the rms of the final is similar to the original
-finalRms = rms(finalWav);
-disp('rms of the final wav file')
-disp(finalRms)
+    % check that the rms of the final is similar to the original
+    finalRms = rms(finalWav);
+    disp('rms of the final wav file');
+    disp(finalRms);
 
-audiowrite([targetWavFn(1:end-4), '_rms.wav'],finalWav,referenceFs)
+    audiowrite([targetWavFn(1:end-4), '_rms.wav'], finalWav, eferenceFs);
 
-%% plot the reference wav and final wav files
-figure()
-subplot(2,1,1)
-plot(referenceWav(:,1),'r')
-hold on
-plot(referenceWav(:,2),'b')
-title('Reference wav file')
+    %% plot the reference wav and final wav files
+    figure()
+    subplot(2, 1, 1)
+    plot(referenceWav(:,1), 'r')
+    hold on
+    plot(referenceWav(:,2), 'b')
+    title('Reference wav file')
 
-subplot(2,1,2)
-plot(finalWav(:,1),'r')
-hold on
-plot(finalWav(:,2),'b')
-title('Final wav file')
+    subplot(2, 1, 2)
+    plot(finalWav(:,1), 'r')
+    hold on
+    plot(finalWav(:,2), 'b')
+    title('Final wav file')
 
 end
